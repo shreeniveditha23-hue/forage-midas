fdc2ae61c5650c9051d99515016467ade718cce6
root <root@BT-21051307>
2025-12-12 05:10:53 +0530

    Fix: use environment bootstrap servers for Kafka consumer factory (embedded test compatibility)

--- src/main/java/com/jpmc/midascore/KafkaConsumerConfig.java
+++ src/main/java/com/jpmc/midascore/KafkaConsumerConfig.java
@@ -3,10 +3,12 @@ package com.jpmc.midascore;
 import com.jpmc.midascore.foundation.Transaction;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -14,27 +16,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConsumerConfig {
+public class KafkaConsumerConfig implements EnvironmentAware {
+
+    private Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
-    public ConsumerFactory<String, Transaction> consumerFactory() {
+    public DefaultKafkaConsumerFactory<String, Transaction> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
 
-        // REQUIRED: Fixes "No group.id found"
+        // Required group.id for Kafka consumer group management
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "midas-core-group");
 
+        // Deserializers
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
+        // Try to detect bootstrap servers from environment (embedded Kafka test helper sets one of these)
+        String bs = null;
+        if (environment != null) {
+            bs = environment.getProperty("spring.kafka.bootstrap-servers");
+            if (bs == null || bs.isBlank()) {
+                bs = environment.getProperty("spring.embedded.kafka.brokers");
+            }
+        }
+        if (bs != null && !bs.isBlank()) {
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bs);
+        }
+
         JsonDeserializer<Transaction> deserializer = new JsonDeserializer<>(Transaction.class);
         deserializer.addTrustedPackages("*");
 
-        return new DefaultKafkaConsumerFactory<>(
-                props,
-                new StringDeserializer(),
-                deserializer
-        );
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
